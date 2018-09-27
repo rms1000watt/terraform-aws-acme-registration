9fb55028da860e72197c22972f1f5492f6ac5359
Ryan Smith <rms1000watt@gmail.com>
2018-09-26 23:11:04 -0700

    moved location of key

--- local.tf
+++ local.tf
@@ -2,5 +2,5 @@ locals {
   email  = "${var.email}"
   bucket = "${var.bucket}"
 
-  name_registration_account_key_pem = "${local.email}.key.pem"
+  name_registration_account_key_pem = "accounts/${local.email}.key.pem"
 }
